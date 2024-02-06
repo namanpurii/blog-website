3585c50f4d9080ee982a4538de9fad2ebfda2489
namanpurii <puri2001naman@gmail.com>
2024-02-07 01:08:55 +0530

    db data fetched and rendered on homepage

--- routes/blog.js
+++ routes/blog.js
@@ -1,41 +1,50 @@
-import express from "express"
+import express from "express";
 import Blog from "../models/blog.js";
 
-const router = express.Router()
-router.use(express.json()) //to parse any incoming request as JSON -> JS Object
+const router = express.Router();
+router.use(express.json()); //to parse any incoming request as JSON -> JS Object
 
 router.get("/new", (req, res) => {
   res.render("form");
 });
 
-router.post("/new", async (req, res) =>{
-    let blog = new Blog({
-        title: req.body.title,
-        description: req.body.description,
-        content: req.body.content,
-    })
-    try{
-        blog = await blog.save() //saves to a collection you created in your MongoAtlas Dashboard.
-        res.redirect(`http://localhost:3000/api/blog/${blog._id}`);
-    } catch(err) {
-        console.log(err)
-        res.render("form", {blog: blog})
+router.post("/new", async (req, res) => {
+  let blog = new Blog({
+    title: req.body.title,
+    description: req.body.description,
+    content: req.body.content,
+  });
+  try {
+    blog = await blog.save(); //saves to a collection you created in your MongoAtlas Dashboard.
+    res.redirect(`http://localhost:3000/api/blog/${blog._id}`);
+  } catch (err) {
+    console.log(err);
+    res.render("form", { blog: blog });
+  }
+});
+
+router.get("/:blogId", async (req, res) => {
+  const blogId = req.params.blogId
+  try {
+    const document = await Blog.findById(blogId).exec();
+    if (document) {
+      res.send(document);
+    } else {
+      res.sendStatus(404);
     }
-})
+  } catch (err) {
+    console.log("Failed retreiving documents" + err);
+    res.sendStatus(500);
+  }
+  // res.sendStatus(200);
+});
 
-router.get("/:blogId", (req, res) =>{
-    const blogId = parseInt(req.params.blogId)
-    // console.log(blogId)
-    res.sendStatus(200)
-})
+router.delete("/:blogId/delete", (req, res) => {
+  res.status(200).json({ msg: `The HTTP method is: ${req.method}` });
+});
 
-router.delete("/:blogId/delete", (req, res)=>{
-    res.status(200).json({msg: `The HTTP method is: ${req.method}`})
-}) 
+router.put("/:blogId/edit", (req, res) => {
+  res.status(200).json({ msg: `The HTTP method is: ${req.method}` });
+});
 
-router.put("/:blogId/edit", (req, res)=>{
-    res.status(200).json({ msg: `The HTTP method is: ${req.method}` });
-})
-
-
-export default router
+export default router;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,50 @@
-import express from "express"
-import mongoose from "mongoose"
-import { fileURLToPath } from "url"
-import path, {dirname} from "path"
-import blogRoute from "../routes/blog.js"
-import { mockBlogs } from "../data.js"
+import express from "express";
+import mongoose from "mongoose";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import blogRoute from "../routes/blog.js";
+// import { mockBlogs } from "../data.js";
+import Blog from "../models/blog.js";
 
-const server = express()
+const server = express();
 
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3000;
 
 mongoose.connect(
   "mongodb+srv://blog:password@cluster0.cjh9kwx.mongodb.net/?retryWrites=true&w=majority"
   // this helps connect to your MongoAtlas Cloud Cluster.
 );
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 //Set EJS as the view engine
-server.set("view engine", "ejs")
-server.set("views", path.join(__dirname, "..", "views"))
+server.set("view engine", "ejs");
+server.set("views", path.join(__dirname, "..", "views"));
 
-server.use(express.urlencoded({ extended: false })) //generally there are errors with parsing post requests at the server-side. This middleware comes handy in that case.
-server.use("/api/blog", blogRoute)
+server.use(express.urlencoded({ extended: false })); //generally there are errors with parsing post requests at the server-side. This middleware comes handy in that case.
+server.use("/api/blog", blogRoute);
 
-server.get('/api/blogs', (req, res) =>{
-    res.render("index", {mockBlogs: mockBlogs
-    }) //renders ../views/index.ejs
-})
+server.get("/api/blogs", async (req, res) => {
+  try {
+    const documents = await Blog.find({}).exec();
+    res.render("index", { documents: documents }); //renders ../views/index.ejs
+  } catch (err) {
+    console.error(err);
+  }
+});
 
-server.listen(port, ()=>{
-    console.log(`Blog server listening on port ${port}`);
-})
+server.listen(port, () => {
+  console.log(`Blog server listening on port ${port}`);
+});
+
+//Validating connection to database cluster
+var conn = mongoose.connection;
+conn.on("connected", () => {
+  console.log("MongoAtlas Cluster Connected");
+});
+conn.on("disconnected", () => {
+  console.log("MongoAtlas Cluster Disconnected");
+});
+
+conn.on("error", console.error.bind(console, "connection error"));
